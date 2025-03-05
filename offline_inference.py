d45f2acf4f6368de914c5e484b58c0117eddf360
Gang Wang (SPEECH) <gaw@microsoft.com>
2025-03-05 10:23:28 +0800

    update inference parameters

--- offline_inference.py
+++ offline_inference.py
@@ -1,5 +1,6 @@
-import torchaudio
 import argparse
+import os
+import torchaudio
 from stepaudio import StepAudio
 
 
@@ -7,6 +8,9 @@ def main():
     parser = argparse.ArgumentParser(description="StepAudio Offline Inference")
     parser.add_argument(
         "--model-path", type=str, required=True, help="Base path for model files"
+    )
+    parser.add_argument(
+        "--output-path", type=str, required=True, help="Base path for response audio files"
     )
     args = parser.parse_args()
 
@@ -16,24 +20,37 @@ def main():
         llm_path=f"{args.model_path}/Step-Audio-Chat",
     )
 
+    output_path = args.output_path
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
+
     # example for text input
+    input_text = "你好，我是你的朋友，我叫小明，你叫什么名字？"
     text, audio, sr = model(
-        [{"role": "user", "content": "你好，我是你的朋友，我叫小明，你叫什么名字？"}],
+        [{"role": "user", "content": input_text}],
         "Tingting",
     )
-    torchaudio.save("output/output_e2e_tqta.wav", audio, sr)
+    print(f"input text: {input_text}")
+    print(f"response text: {text}")
+    response_audio_path1 = f"{output_path}/output_e2e_tqta1.wav"
+    torchaudio.save(response_audio_path1, audio, sr)
+    # torchaudio.save("output/output_e2e_tqta.wav", audio, sr)
 
     # example for audio input
     text, audio, sr = model(
         [
             {
                 "role": "user",
-                "content": {"type": "audio", "audio": "output/output_e2e_tqta.wav"},
+                # "content": {"type": "audio", "audio": "output/output_e2e_tqta.wav"},
+                "content": {"type": "audio", "audio": response_audio_path1},
             }
         ],
         "Tingting",
     )
-    torchaudio.save("output/output_e2e_aqta.wav", audio, sr)
+    print(f"response text: {text}")
+    response_audio_path2 = f"{output_path}/output_e2e_aqta2.wav"
+    torchaudio.save(response_audio_path2, audio, sr)
+    # torchaudio.save("output/output_e2e_aqta.wav", audio, sr)
 
 
 if __name__ == "__main__":
